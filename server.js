8244978eb15a5d6c5b17059829499b98c619a123
olasmartTech <eweleyeolamide10@gmail.com>
2023-09-11 11:41:02 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+// {
+//     "slack_name": "example_name",
+//     "current_day": "Monday",
+//     "utc_time": "2023-08-21T15:04:05Z",
+//     "track": "backend",
+//     "github_file_url": ,
+//     "github_repo_url":
+//     "status_code": 200
+//   }
+
+const app = express();
+app.use(express.json());
+
+let days = ['sunday', 'monday', 'tuesday', 'wednessday', 'thursday', 'friday', 'saturday']
+const date = new Date()
+const currentDay = days[date.getDay()];
+current_time = date.toISOString();
+
+// test route
+const name = 'Eweleye olamide';
+const myTrack = 'Backend';
+file_url = "https://github.com/username/repo/blob/main/file_name.ext"
+repo_url =  "https://github.com/username/repo",
+
+app.get('/api', (req, res) => {
+    const  { slack_name, track } = req.query
+    res.status(200).json({
+        data: {
+            slack_name: slack_name && name,
+            current_day: currentDay,
+            utc_time: current_time,
+            track: track && myTrack,
+            github_file_url: file_url,
+            github_repo_url: repo_url,
+            status_code: 200,
+        } 
+    })
+})
+
+app.listen(3030, () => console.log('server running on port 3030'));
