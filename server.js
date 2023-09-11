fa12673e95ab9ac590a765c29c2ff3f4deff6942
olasmartTech <eweleyeolamide10@gmail.com>
2023-09-11 15:34:03 -0700

    modify logic

--- server.js
+++ server.js
@@ -3,25 +3,24 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-let days = ['sunday', 'monday', 'tuesday', 'wednessday', 'thursday', 'friday', 'saturday']
-const date = new Date()
-const currentDay = days[date.getDay()];
-current_time = date.toISOString();
-
-// test route
-const name = 'Eweleye olamide';
-const myTrack = 'Backend';
-file_url = "https://github.com/olasmart18/zuri_HNGx-stage1/blob/main/server.js"
-repo_url =  "https://github.com/olasmart18/zuri_HNGx-stage1",
-
+// GET route to acceept query
 app.get('/api', (req, res) => {
+    let days = ['sunday', 'monday', 'tuesday', 'wednessday', 'thursday', 'friday', 'saturday']
+    const date = new Date()
+    const currentDay = days[date.getDay()];
+    current_time = date.toISOString();
+    const name = 'Eweleye olamide';
+    const myTrack = 'Backend';
+    file_url = "https://github.com/olasmart18/zuri_HNGx-stage1/blob/main/server.js";
+    repo_url =  "https://github.com/olasmart18/zuri_HNGx-stage1";
+
     const  { slack_name, track } = req.query
     res.status(200).json({
         data: {
-            slack_name: slack_name,
+            slack_name: slack_name && name,
             current_day: currentDay,
             utc_time: current_time,
-            track: track.toLowerCase(),
+            track: track && myTrack.toLowerCase(),
             github_file_url: file_url,
             github_repo_url: repo_url,
             status_code: 200,
@@ -29,4 +28,4 @@ app.get('/api', (req, res) => {
     })
 })
 
-app.listen(3000, () => console.log('server running on port 3030'));
+app.listen(3000, () => console.log('server running on port 3000'));
